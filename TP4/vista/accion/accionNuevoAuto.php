5ebd1897655a665cd583f1d5d941e61ac308fbb0
JuanF368 <juanfarroni1314@gmail.com>
2024-09-14 23:29:12 -0300

    opcion de agregar a la persona si es que no existe cuando se trata de ingresar un nuevo auto

--- TP4/vista/accion/accionNuevoAuto.php
+++ TP4/vista/accion/accionNuevoAuto.php
@@ -8,15 +8,21 @@ include_once '../../util/funciones.php';
 $datos = data_submitted();
 
 $yaExiste = true;
+$personaExiste=false;
 $objAuto = new AbmAuto();
 $objPersona = new AbmPersona();
 $param = ["patente" => $datos["patente"]];
 if (empty($objAuto->buscar($param))) {
-    $objAuto->alta($datos);
+    $yaExiste = false;
+}
+if(!empty($objPersona->buscar(["nroDni" => $datos["dniDuenio"]]))){
+    $personaExiste=true;
+}
+if($personaExiste && !$yaExiste){
+    //$objAuto->alta($datos);
     $auto=$objAuto->buscar($param);
     $datosDuenio=$objPersona->buscar(["nroDni" => $auto[0]->getObjPersona()]);
     $duenio=$datosDuenio[0]->getNombre()." ".$datosDuenio[0]->getApellido();
-    $yaExiste = false;
 }
 
 ?>
@@ -24,10 +30,15 @@ if (empty($objAuto->buscar($param))) {
 <body>
     <div class="container p-4 my-4 d-flex justify-content-center">
         <div class="div-form">
-            <?php if ($yaExiste): ?>
+            <?php if(!$personaExiste){?>
+                <p class="display-6" id="tituloEjercicio">ERROR</p>
+                <p>No existe ninguna persona con este numero de DNI</p>
+                <a class="btn mt-3 bg-success text-white" href="../nuevaPersona.php" id="botonMenu">Agregar persona</a><br>
+            <?php }?>
+            <?php if ($yaExiste){?>
                 <p class="display-6" id="tituloEjercicio">ERROR</p>
                 <p>Ya existe un auto registrado con esta patente, intente nuevamente.</p>
-            <?php else: ?>
+            <?php }  if($personaExiste && !$yaExiste){?>
                 <?php  ?>
                 <p class="display-6" id="tituloEjercicio">Auto ingresado</p>
                 <p>Datos del auto ingresado:</p>
@@ -37,7 +48,7 @@ if (empty($objAuto->buscar($param))) {
                     <li>Modelo: <?php echo $datos["modelo"]; ?></li>
                     <li>Dueño: <?php echo $duenio; ?></li>
                 </ul>
-            <?php endif; ?>
+            <?php }?>
             <a class="btn mt-3 text-white" href="../nuevoAuto.php" id="botonMenu">Volver atrás</a>
             <a class="btn mt-3 text-white bg-dark" href="../../../index.php">Volver al menú principal</a>
         </div>
